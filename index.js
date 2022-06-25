20ead7b42b6a3beeebf820b3d04c18c699c380c8
= <=>
2022-06-25 15:09:43 +0700

    4th commit pre-final

--- index.js
+++ index.js
@@ -1,22 +1,88 @@
-const PORT = 8000;
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const PORT = process.env.PORT || 1000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+const newspapers = [{
+        name: 'newsbtc',
+        address: 'https://www.newsbtc.com',
+        base: ''
+    },
+    {
+        name: 'investorplace',
+        address: 'https://investorplace.com/',
+        base: ''
+    },
+    {
+        name: 'cryptolian',
+        address: 'https://www.cryptopolitan.com/news/',
+        base: '',
+    },
+    {
+        name: 'coinquora',
+        address: 'https://coinquora.com',
+        base: '',
+    },
+    {
+        name: 'cryptonomist',
+        address: 'https://en.cryptonomist.ch/categoria/crypto/bitcoin-en/',
+        base: '',
+    },
+    {
+        name: 'cryptodaily',
+        address: 'https://cryptodaily.co.uk',
+        base: '',
+    },
+    {
+        name: 'utoday',
+        address: 'https://u.today/bitcoin-news',
+        base: '',
+    }
+]
+const articles = []
+    // lookup = [{
+    //     lookupname: "bitcoin"
+    // }, {
+    //     lookupname: "BTC"
+    // }, {
+    //     lookupname: "Bitcoin"
+    // }]
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
 
-const app = express();
-app.get('/', (req, res) => {
-    res.json('Welcome to my Crypto news API')
+            $('a:contains("Bitcoin")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+
+        })
 })
+app.get('/', (req, res) => {
+    res.json('Welcome to my bitcoin News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
 app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperAddr = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
     const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
-    axios.get(newspaperAddress)
+    axios.get(newspaperAddr)
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
             const specificArticles = []
-            $('a:contains("bitcoin")', html).each(function() {
+            $('a:contains("Bitcoin")', html).each(function() {
                 const title = $(this).text()
                 const url = $(this).attr('href')
                 specificArticles.push({
@@ -28,4 +94,4 @@ app.get('/news/:newspaperId', (req, res) => {
             res.json(specificArticles)
         }).catch(err => console.log(err))
 })
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ` + PORT))
