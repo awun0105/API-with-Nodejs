7c09b286520ec15f9c423fac51b9d303ce68024f
= <=>
2022-06-25 08:13:31 +0700

    3th commit

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const cheerio = require('cheerio');
 
 const app = express();
 app.get('/', (req, res) => {
-    res.json('Welcome to my Crypto API');
+    res.json('Welcome to my Crypto news API')
 })
-app.get('/News', (req, res) => {
-    axios.get('https://www.tradingview.com/chart/EiM5N1VF/?symbol=BINANCE%3ALITUSDT')
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+    axios.get(newspaperAddress)
         .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            $('a:contain')
-        });
-});
-app.listen(PORT, () => console.log('server running on PORT ' + PORT))
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+            $('a:contains("bitcoin")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
